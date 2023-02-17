9eb88108ceb43ee7b57f7800c08235d8a6560516
Skander <msalmi.skander@esprit.tn>
2023-02-17 20:44:55 +0100

    setting up environement
    create react app
    create node with express app
    connect in proxy

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <h1>Sahtek</h1>
+    </div>
+  );
+}
+
+export default App;
