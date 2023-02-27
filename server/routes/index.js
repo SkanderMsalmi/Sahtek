2868b978bade39bd921a56e5965a1081ab01515d
SarraTounsi <sarra.tounsi@esprit.tn>
2023-02-27 21:34:17 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,13 @@
 const router =require('express').Router();
 const apiRouter = require('./api');
+const ProductResolvers = require("./shop/product/resolver");
 
+ 
+
+module.exports= [
+    ProductResolvers,
+    
+]
 router.use('/api',apiRouter);
 
 module.exports = router;
