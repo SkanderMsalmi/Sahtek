9fa6bf5bdb83d8d424929fb114d2880ae8a1b9c1
SarraTounsi <sarra.tounsi@esprit.tn>
2023-03-08 09:56:23 +0100

    front

--- client/src/pages/Signin/Login.js
+++ client/src/pages/Signin/Login.js
@@ -1,5 +1,6 @@
 function Login (){
     return (
+<<<<<<< Updated upstream
         <div className="container">
             <div className="item">
                 <h2 className="logo">Sahtek</h2>
@@ -7,6 +8,36 @@ function Login (){
                     <h2>Welcome ! <br/><span>To Our Website</span></h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam assumenda minus, laborum nisi doloribus eius dicta voluptates numquam nulla cum?</p>
                 </div>
+=======
+        <section>
+            <div className={styles.formBox}>
+                <form onSubmit={handleSubmit}>
+                    <h2 style={{color:"#fff"}}>Login</h2>
+                    <div className={styles.inputbox}>
+                    <i className="fa-sharp fa-regular fa-envelope"></i>
+                        <input type="text" className={styles.userInput} required value={email} onChange={(e)=> setEmail(e.target.value)} />
+                        <label htmlFor="email" className={styles.userLabel} >Email</label>
+                    </div>
+                    <div className={styles.inputbox}>
+                    <i className="fa-solid fa-lock"></i>
+                        <input type="password"  className={styles.userInput}  required value={password} onChange={(e)=> setPassword(e.target.value)} />
+                        <label className={styles.userLabel} htmlFor="password" >Password</label>
+                    </div>
+                    <div className={styles.forget+" form-check"}>
+                        <label className="lab form-check-label" htmlFor="rm"><input type="checkbox"  id="rm" className="form-check-input"/>Remeber Me <a href="/forgetpassword" style={{marginLeft:"15px"}}> Forget Password </a></label>
+                        
+                    </div>
+                    <button>Login</button>
+                    <div className={styles.register}>
+                        <p>Don't have an account ? <a href="#">Register</a></p>
+                        </div>
+                    {loading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+      {data && data.login.success && <p>Login successful!</p>}
+      {data && !data.login.success && <p>{data.login.message}</p>}
+                </form>
+
+>>>>>>> Stashed changes
             </div>
         </div>
     )
--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {createBrowserRouter} from 'react-router-dom';
 import App from './App';
+<<<<<<< Updated upstream
+=======
+import Profile from './pages/Profile/Profile';
+import Register from './pages/Register/Register';
+import Login from './pages/Signin/Login';
+import Forgotpassword from './pages/forgotPassword/forgotPassword';
+import Resetpassword from './pages/ResetPassword/resetPassword';
+>>>>>>> Stashed changes
 const Homepage = React.lazy(()=>import("./pages/Homepage/Homepage"));
 // const Signup = React.lazy(()=>import("./pages/Signup/Signup"));
 export const router = createBrowserRouter([
@@ -12,7 +20,38 @@ export const router = createBrowserRouter([
             {
                 index:true,
                 element: <Homepage />
+<<<<<<< Updated upstream
             }
+=======
+            },
+            {
+              path: 'login',
+              element: <Login />,
+            },
+            {
+              path: 'register',
+              element: <Register />,
+            },
+            {
+              path: 'profile',
+              element: (
+                  <Profile />
+              ),
+            },
+            {
+              path: 'forgetpassword',
+              element: (
+                      <Forgotpassword />
+                ),
+            },
+            {
+              path: 'resetpassword',
+              element: (
+                      <Resetpassword/>
+                ),
+            },
+      
+>>>>>>> Stashed changes
       
         ]
     }
