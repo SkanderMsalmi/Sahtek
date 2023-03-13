0a92e67fc5fc1a1c664993af1d3eb923c09a6e31
Skander <msalmi.skander@esprit.tn>
2023-03-13 19:03:33 +0100

    alert and resolver

--- server/routes/users/resolver.js
+++ server/routes/users/resolver.js
@@ -43,7 +43,8 @@ const resolvers = {
           role,
           profileImage,
           name,
-          dateOfBirththerapist: {},
+          dateOfBirth,
+          therapist: {},
         });
       }
       //send email verification
