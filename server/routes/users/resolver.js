fc8c2a042a96fec89e26ab4f00696d907d7a23cc
MAYMA1 <98560777+MAYMA1@users.noreply.github.com>
2023-03-15 10:06:09 +0100

    reset password mail template

--- server/routes/users/resolver.js
+++ server/routes/users/resolver.js
@@ -240,7 +240,51 @@ const resolvers = {
         from: "sahtek2023@gmail.com",
         to: email,
         subject: "Reset Password Link",
-        text: `Please click on the following link to reset your password: http://localhost:3000/resetPassword/${user.id}/${token}`,
+        // text: `Please click on the following link to reset your password: http://localhost:3000/resetPassword/${user.id}/${token}`,
+        html: `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Document</title>
+
+				 
+			
+			 
+				  
+			</head>
+			
+			<body  style=" min-height: 60vh;    
+			background: #cdf0f0;    padding-left: 27%;  padding-top: 10%" >
+		 
+			<form class="verify-form" style="      padding: 40px;  
+
+				border-radius: 15px;
+				width: 400px;  background: #ffffff;" >
+					<h3   style="font-size: 25px; font-weight: 700; letter-spacing: -1px; line-height: 48px;  color: #403D39;  
+					  margin-bottom : 15px;
+						margin-top:20px;">Reset password </h3>
+					
+					 
+					  <p   style=" margin-bottom: 0px; font-size: 17px;
+				  color: #1b1b1b;  ">Please click on the following link to reset your password.</p>
+					 <a   href="http://localhost:3000/resetPassword/${user.id}/${token}">
+					   <button style="width: 100%;
+					   margin-top: 40px;
+						   margin-bottom: 20px;
+				  
+					  background   :#51cbce ;
+					   color: #ffffff;
+					  padding:15px;
+					font-size: 17px;
+					  border-radius: 15px;  border-width: 0px"  class="btn btn-primary ">Reset</button></a>
+				   </form> 
+			 
+				
+				
+			</body>
+			</html> `
       };
       await transporter.sendMail(mailOptions);
       return true;
