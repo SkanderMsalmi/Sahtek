50d6127a49cb7165707a5f8a93b1f849769dbb17
ChibaniAdam <adam.chibani@esprit.tn>
2023-04-25 23:01:13 +0100

    changed amazon to carousel

--- client/src/pages/Shop/HomePage/HomeShop.js
+++ client/src/pages/Shop/HomePage/HomeShop.js
@@ -10,7 +10,8 @@ import { selectProducts, selectWishlist } from "../../../store/selectors";
 import { selectCountAll } from '../../../store/shop/cartSlice';
 import { NavLink } from 'react-router-dom';
 import { Link } from "react-router-dom"
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import {
   addToWishlist,
   emptyWishlist,
@@ -209,13 +210,67 @@ const HomeShop = () => {
         <div className="border-top mb-5" style={{ width: "100vw" }}>
           <h4>See also:</h4>
         </div>
-
         {loadingAmazon && <Spinner />}
-        {JSON.parse(dataAmazon?.getAmazonProducts).map((product, index) => (
-          <div key={index} className="col-lg-3 col-md-4 col-sm-6 mb-3">
-            <a href={product.productUrl} target="_blank"> <AmazonProd className={styles.amz} product={product} /></a>
-          </div>
-        ))}
+
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite={false}
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024
+              },
+              items: 3,
+              partialVisibilityGutter: 40
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0
+              },
+              items: 1,
+              partialVisibilityGutter: 30
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464
+              },
+              items: 2,
+              partialVisibilityGutter: 30
+            }
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={3}
+          swipeable
+        >
+          {JSON.parse(dataAmazon?.getAmazonProducts).map((product, index) => (
+
+            <a href={product.productUrl} target="_blank" > <AmazonProd className={styles.amz} product={product} /></a>
+          ))}
+        </Carousel>
+
 
 
       </div>
