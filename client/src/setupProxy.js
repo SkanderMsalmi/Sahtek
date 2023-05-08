3f958d27062e0395fc92189ed168eeda962f1beb
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-05-08 23:21:36 +0100

    proxy middleware

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     "/api/*",
     createProxyMiddleware({
-      target: "http://127.0.0.1:5000",
+      target: "https://sah-tek-serv.onrender.com/graphql",
       secure: false,
     })
   );
